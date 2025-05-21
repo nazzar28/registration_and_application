023517dcd0a68b25fa94c513b879adcc6c0e3eba
nazzar28 <talantbekovnazar28@gmail.com>
2025-05-21 15:23:23 +0600

    first

---
+++ src/main/java/com/example/registration_and_application/RegistrationAndApplication.java
@@ -0,0 +1,13 @@
+package com.example.registration_and_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registration_and_application/RegistrationAndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registration_and_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
